1c8a321640bc5de8330e20fdc5d7fbbc49ef7a2f
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:33:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_10_A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var x1 = double.Parse(input[0]);
+        var y1 = double.Parse(input[1]);
+        var x2 = double.Parse(input[2]);
+        var y2 = double.Parse(input[3]);
+
+        var l = Math.Pow(y2 - y1, 2);
+        var r = Math.Pow(x2 - x1, 2);
+
+        var ans = Math.Sqrt(l + r);
+        Console.WriteLine(ans);
+    }
+}
